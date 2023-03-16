892854a1a2492b01d14eab87b4f05c23237e074a
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-03-16 23:10:18 +0100

    pagina de login semi-hecha, pagina de registro(por si se necesita al final) semi-hecha. Navbar y css del navbar implementado

--- restaurante-pf-front/src/App.js
+++ restaurante-pf-front/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage/LandingPage';
+import IniciarSesion from './components/IniciarSesion/IniciarSesion';
+import Register from './components/Register/Register';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">  
+      <div>  
         <Routes>    
-          <Route exact path='/' Component={LandingPage} />
+          <Route exact path='/IniciarSesion' Component={IniciarSesion} />
+          <Route exact path='/Register' Component={Register} />
         </Routes>
       </div>
     </BrowserRouter>
