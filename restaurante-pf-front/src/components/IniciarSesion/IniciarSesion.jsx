209d864836e87725d48d20915579dbe677f44c56
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-03-17 04:33:47 +0100

    login con google medio montao

--- restaurante-pf-front/src/components/IniciarSesion/IniciarSesion.jsx
+++ restaurante-pf-front/src/components/IniciarSesion/IniciarSesion.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import "../SASS/IniciarSesion.modules.css"
+import { GoogleLogin } from '@react-oauth/google';
+import '../SASS/register.modules.css'
 
 
 
 const IniciarSesion = () => {
+
+    const responseMessage = (response) => {
+        console.log(response)
+    };
+
+    const errorMessage = (error) =>  {
+        console.log(error)
+    }
+
     return (
-        <div>
+        <div className='login-bg'>
             <div>
                 <NavBar />
             </div> 
@@ -16,12 +27,14 @@ const IniciarSesion = () => {
             <div className='main-container'>
                 <p>¿Cúal es tú número de teléfono o correo electrónico?</p>
 
-                <form>
+                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+
+                <form className=''>
                     <input type="text" name="mail_tlf" placeholder="Ingrese su número o correo"></input>
                     <button>Continuar</button>
                 </form>
                 
-                <Link to="/register"><button>Registarse</button></Link>
+                <Link to="/register"><button className='register-btn'>Registarse</button></Link>
                 
             </div>
         </div>  
--- restaurante-pf-front/src/index.js
+++ restaurante-pf-front/src/index.js
@@ -4,19 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 // import { store } from "./redux/store"
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <GoogleOAuthProvider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
