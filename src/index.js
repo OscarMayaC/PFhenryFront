6e7a5e064a3f201844251bc1f3c3a0d4c6cdf092
HassanOcar <hassan_ocar@hotmail.com>
2023-03-18 13:53:53 -0300

    subida de landing-delivery

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Pages/App';
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+<BrowserRouter> 
+
+   <App />
+
+</BrowserRouter>
+
+,
+  document.getElementById('root')
+);
+
