8316a3af714035156b51561618b0a56aaf34cd10
Clayman7 <clayman.asg@gmail.com>
2023-03-18 12:43:37 -0600

    hecho

--- restaurante-pf-front/src/App.js
+++ restaurante-pf-front/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
+import Testimonios from './components/Testimonios/Testimonios';
+import Nosotros from './components/SobreNosotros/Nosotros';
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <div className="App">  
         <Routes>    
           <Route exact path='/' Component={LandingPage} />
+          <Route exact path='/reseñas' Component={Testimonios} />
+          <Route exact path='/sobrenosotros' Component={Nosotros} />
+
         </Routes>
       </div>
     </BrowserRouter>
