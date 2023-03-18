7142781ab3b16703c091157488bc4158f775e839
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-03-18 19:23:35 -0300

    Refacciones de Json, esta andando

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,32 +1,26 @@
-import './Styles/App.css';
-import React from 'react';
-import { Route } from 'react-router-dom'
-import Landing from '../Components/Landing'
-import NavBar from '../Components/NavBar';
-import Delivery from '../Components//Delivery';
-import Testimonios from '../Components/Testimonios/Testimonios';
-import Nosotros from '../Components/SobreNosotros/Nosotros';
-import IniciarSesion from '../Components/IniciarSesion/IniciarSesion';
-import Register from '../Components/Register/Register';
+import "./Styles/App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Landing from "../Pages/Landing";
+import NavBar from "../Components/NavBar";
+import Delivery from "../Pages/Delivery";
+import Testimonios from "../Components/Testimonios/Testimonios.jsx";
+import Nosotros from "../Components/SobreNosotros/Nosotros";
+import IniciarSesion from "../Components/IniciarSesion/IniciarSesion";
+import Register from "../Components/Register/Register";
 
 function App() {
+  return (
+    <div className="App">
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/delivery" component={NavBar} />
+      <Route exact path="/delivery" component={Delivery} />
+      <Route exact path="/reseñas" component={Testimonios} />
+      <Route exact path="/sobrenosotros" component={Nosotros} />
+      <Route exact path="/IniciarSesion" component={IniciarSesion} />
+      <Route exact path="/Register" component={Register} />
+    </div>
+  );
+}
 
-    return (
-      
-      <div className="App">
-    
-   
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/delivery' component={NavBar} />
-        <Route exact path='/delivery' component={Delivery} />
-        <Route exact path='/reseñas' Component={Testimonios} />
-        <Route exact path='/sobrenosotros' Component={Nosotros} />
-        <Route exact path='/IniciarSesion' Component={IniciarSesion} />
-        <Route exact path='/Register' Component={Register} />
-  
-      </div>
-    );
-  }
-  
-  export default App;
-  
+export default App;
