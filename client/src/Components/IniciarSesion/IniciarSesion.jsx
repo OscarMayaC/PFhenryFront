5abc36b86a34b8b1d36a07bc2672e027b5298350
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-03-24 18:46:28 +0100

    registro hecho y login hecho(falta por crear el perfil donde traeran los datos)

--- client/src/Components/IniciarSesion/IniciarSesion.jsx
+++ client/src/Components/IniciarSesion/IniciarSesion.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
-// import { loginUser } from '../../redux/actions';
+import { getUserByLogin } from '../../redux/actions';
 import "../SASS/IniciarSesion.modules.css"
 import '../SASS/register.modules.css'
 
@@ -10,25 +10,38 @@ import '../SASS/register.modules.css'
 
 const IniciarSesion = () => {
 
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     // const history = useHistory();
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const [input, setInput] = useState({
-        correo: "",
-        contraseña: "",
-    })
+    // const [input, setInput] = useState({
+    //     email: "",
+    //     password: "",
+    // })
 
-    function handleChange(e) {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
-    }
+    // function handleChange(e) {
+    //     setEmail({
+    //         ...email,
+    //         [e.target.name]: e.target.value
+    //     })
+    //     setPassword({
+    //         ...password,
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     dispatch(getUserByLogin(email, password))
+    //     console.log(getUserByLogin())
+    // }
 
     const handleSubmit = (e) => {
-        console.log("que ise el tio")        
-    }
+        e.preventDefault();
+        dispatch(getUserByLogin(email, password));
+      };
 
     return (
         <div className='login-bg'>
@@ -39,19 +52,19 @@ const IniciarSesion = () => {
                         <label>Correo: </label>
                             <input className='body-input'
                                 type="email"
-                                value={input.correo}
-                                name= "correo"
+                                value={email}
+                                name= "email"
                                 placeholder='Ingrese su correo...'
-                                onChange={handleChange}
+                                onChange={(e) => setEmail(e.target.value)}
                             />                  
                    
                         <label>Contraseña: </label>
                             <input className='body-input'
                                 type="password"
-                                value={input.contraseña}
-                                name= "contraseña"
+                                value={password}
+                                name= "password"
                                 placeholder='Ingrese su contraseña...'
-                                onChange={handleChange}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                     </div>
                     <br />
