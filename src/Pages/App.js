ac017037c8f5026c8f3a1c63979e6d2a0ea50a9c
Clayman7 <clayman.asg@gmail.com>
2023-03-25 13:47:55 -0600

    Componente Detalles

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -4,10 +4,11 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "../Pages/Landing";
 import NavBar from "../Components/NavBar";
 import Delivery from "../Pages/Delivery";
-import Testimonios from "../Components/Testimonios/Testimonios.jsx";
+import Reseñas from "../Components/Reseñas/Reseñas";
 import Nosotros from "../Components/SobreNosotros/Nosotros";
-import IniciarSesion from "../Components/IniciarSesion/IniciarSesion";
-import Register from "../Components/Register/Register";
+import IniciarSesion from "../components/IniciarSesion/IniciarSesion";
+import Register from "../components/Register/Register";
+import Detalles from "../Components/Detail/details";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
       <Route exact path="/" component={Landing} />
       <Route exact path="/delivery" component={NavBar} />
       <Route exact path="/delivery" component={Delivery} />
-      <Route exact path="/reseñas" component={Testimonios} />
+      <Route exact path="/reseñas" component={Reseñas} />
       <Route exact path="/sobrenosotros" component={Nosotros} />
       <Route exact path="/IniciarSesion" component={IniciarSesion} />
       <Route exact path="/Register" component={Register} />
+      <Route exact path="/detalles" component={Detalles} />
     </div>
   );
 }
