85b0b42729179a4a2246c0660b89dbb84553c783
lejesterdelacorte <86328255+lejesterdelacorte@users.noreply.github.com>
2023-03-26 15:49:59 -0300

    Revert "Componente Detalles"

--- client/src/Components/SobreNosotros/Nosotros.jsx
+++ client/src/Components/SobreNosotros/Nosotros.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from '../../imgs/logo.png';
+import user from '../../imgs/user.png'
 import '../css/NavBar.modules.css';
 import '../css/nosotros.css';
-import NavBar from '../../components/NavBar/NavBar';
-
+import { Link } from "react-router-dom";
 
 
 const Nosotros = () => {
@@ -12,7 +12,37 @@ const Nosotros = () => {
 
     return (
         <div className=''>
-            <div><NavBar/></div>
+            <div className='navbar-main-contenedor'>
+
+                <div className='navbar-left-zone'>
+                    <Link to="/">
+                        <div className='navbar-circulo-logo-fondo'>
+                            <div className='circulo-navbar'>
+                                <img src={logo} className="navbar-logo-left" alt="logo" />
+                            </div>
+
+                        </div>
+                    </Link>
+
+                    <h1 className='logo-texto-navbar-left'>Rosa del Viento</h1>
+                </div>
+
+                <div className='navbar-right-zone'>
+
+                    <Link to={"/sobrenosotros"}> <button className='navbar-button-sobre-nosotros'>Sobre nosotros</button> </Link>
+                    <Link to={"/reseñas"}> <button className='navbar-button-reseñas'>Reseñas</button> </Link>
+                    <Link to={"/iniciarsesion"}> <button className='navbar-button-iniciar-sesion'>
+                        <div className='contenedor-button-navbar-inciarsesion'>
+
+                            <img src={user} alt="img-user" className="navbar-iniciar-user-icon" width="40px" height="40px"></img>
+                            <h1 className='txt-navbar-iniciarsesion'>¡Iniciar sesión!</h1>
+
+                        </div>
+                    </button>
+                    </Link>
+                </div>
+
+            </div>
 
             <div className='base'>
 
